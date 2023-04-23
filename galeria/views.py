41a24e5c33f932320fda887e897e6f69ddb49e0e
Gabriel Santana <prof.gabrielsantana@gmail.com>
2023-04-22 21:14:18 -0300

    criação da lista de dados

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
 
+
 def index(request):
-    return render(request, 'index.html')
+    dados = {
+    1:{"nome":"Nebulosa de Carina",
+        "legenda":"webbtelescope.org / NASA / James Webb"},
+    2:{"nome":"Galáxia NGC 1079",
+        "legenda":"nasa.org / NASA / Hubble"}
+    }
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
-    return render(request, 'imagem.html')
+    return render(request, 'galeria/imagem.html')
